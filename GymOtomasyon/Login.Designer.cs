50bc3f8ba91676f1bf846a95ea8020cd36948a5a
Kompetankedi <berkbag34@gmail.com>
2024-12-11 14:50:42 +0300

    Tasarım + sql baglanti class oluşturuldu

---
+++ GymOtomasyon/Login.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GymOtomasyon
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.BtnKasiyer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnKasiyer
+            // 
+            this.BtnKasiyer.Image = global::GymOtomasyon.Properties.Resources.kasiyer;
+            this.BtnKasiyer.Location = new System.Drawing.Point(42, 57);
+            this.BtnKasiyer.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnKasiyer.Name = "BtnKasiyer";
+            this.BtnKasiyer.Size = new System.Drawing.Size(348, 331);
+            this.BtnKasiyer.TabIndex = 0;
+            this.BtnKasiyer.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(142, 393);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kasiyer İşlemleri";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::GymOtomasyon.Properties.Resources.customer1;
+            this.button1.Location = new System.Drawing.Point(745, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 333);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(857, 393);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Müşteri İşlemleri";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GymOtomasyon.Properties.Resources.gymwallpapaer;
+            this.ClientSize = new System.Drawing.Size(1130, 615);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnKasiyer);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giriş";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnKasiyer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GymOtomasyon/SqlCon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GymOtomasyon
+{
+    public class SqlCon
+    {
+        public SqlConnection con() 
+        {
+            SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=CarbonFit;Integrated Security=True;Encrypt=False") ;
+            con.Open();
+            return con ;
+        }
+    }
+}
